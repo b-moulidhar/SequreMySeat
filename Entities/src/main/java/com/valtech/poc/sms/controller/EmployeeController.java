e34cd7cd2f120b3477b875d7929fa48933895887
Moyukh Saha <moyukh.saha@valtech.com>
2023-03-27 20:30:23 +0530

    Employee Profile

--- Entities/src/main/java/com/valtech/poc/sms/controller/EmployeeController.java
+++ Entities/src/main/java/com/valtech/poc/sms/controller/EmployeeController.java
@@ -7,15 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.valtech.poc.sms.entities.Employee;
 import com.valtech.poc.sms.service.EmployeeService;
 
 @Controller
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 	
 	@Autowired
 	private EmployeeService employeeService;
+	
+	@GetMapping("/{eId}")
+    public Employee getEmployeeById(@PathVariable int eId) {
+        return employeeService.getEmployeeByeId(eId);
+    }
 	
 	@ResponseBody
 	@GetMapping("/getAllEmployees/{empID}")

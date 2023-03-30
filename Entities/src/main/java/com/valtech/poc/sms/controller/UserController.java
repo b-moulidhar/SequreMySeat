3abccbc4afa5b05f1349ca3f316a37a4a227c306
Anuj S M <anuj.sm@valtech.com>
2023-03-30 18:33:03 +0530

    minor changes

--- Entities/src/main/java/com/valtech/poc/sms/controller/UserController.java
+++ Entities/src/main/java/com/valtech/poc/sms/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ import com.valtech.poc.sms.service.UserService;
 import com.valtech.poc.sms.entities.Roles;
 
 @RestController
+@CrossOrigin(origins = "http://10.191.80.73/:3000")
 public class UserController {
 
 	@Autowired
